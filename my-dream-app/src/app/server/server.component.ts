d27d831880c1158360fb7b302df729178d0feff3
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-01-13 21:46:50 -0200

    Criação do primeiro componente chamado server

---
+++ my-dream-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //Nome/Seletor do Componente, deve ser único em toda a aplicação
+    selector: 'app-server',
+    //URL do HTML do Componente
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
