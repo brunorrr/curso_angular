32a9f70939791ed605abd33f70544e5779607756
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-01-13 23:01:27 -0200

    Customizando as formas de definir os estilos e estrutura dos componentes

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  //Usando styles para criar estilos diretamente no Typescript
+  styles: [`
+    h3{
+      color: lightblue;
+  }
+  `]
 })
 export class AppComponent {
 }
--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //selector: 'app-servers',
+  //Com os colchetes [] colocamos o seletor do app-server como um atributo de algum elemento HTML
+  //selector: '[app-servers]',
+  //Colocando um . na frente definimos que o componente será identificado como classe
+  selector: '.app-servers',
+  //Colocando como template apenas, podemos colocar o código HTML dentro do typescript
+  //templateUrl: './servers.component.html',
+  //Usando ` podemos colocar várias linhas de código para descrever uma linha
+  template: `<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
