66f4303d8d767aeffce78bc5c4fd8311f1cf2ae9
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-02-21 22:57:46 -0300

    Implement style encapsulation

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  /*
+    Defining style encapsulation's value to None
+    On this way 'app' styles will affect 'server-element' elements
+    @see https://angular.io/api/core/ViewEncapsulation
+  */
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   /*
