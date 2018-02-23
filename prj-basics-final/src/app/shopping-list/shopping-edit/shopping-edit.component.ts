b78f45def97d4ae4e678341deb4f2cdc4c444983
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-02-23 00:09:57 -0300

    Implemement add button on shooping list

--- prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdded(){
+    this.ingredientAdded.emit({ 
+      name: this.nameInputRef.nativeElement.value, 
+      amount: this.amountInputRef.nativeElement.value } );
   }
 
 }
