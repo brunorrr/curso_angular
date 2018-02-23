acabc8c6b28405a4698374acb3effd0718df4b19
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-02-22 23:27:14 -0300

    Implement navigation between Recipe Book tabs

--- prj-basics-final/src/app/header/header.component.ts
+++ prj-basics-final/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output() tabChanged = new EventEmitter<number>();
+
+  changeTab( index: number ){
+    this.tabChanged.emit(index);
+  }
 
 }
