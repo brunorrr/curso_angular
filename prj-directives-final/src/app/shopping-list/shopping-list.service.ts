8965c1e0168d83f058f22247b99df529299ac418
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-03-04 13:26:21 -0300

    Implement Shopping list service

--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+
 export class ShoppingListService {
-    
+
+    ingredientChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
+
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+
+    addIngredient(newIngredient: Ingredient){
+        this.ingredients.push(newIngredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
 }
