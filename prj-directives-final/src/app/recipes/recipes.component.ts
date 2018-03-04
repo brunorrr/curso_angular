cd25838d17e24318ee05c3ad8e430e398c9f5c1f
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-03-04 13:08:31 -0300

    Change event emition to use Service event

--- prj-directives-final/src/app/recipes/recipes.component.ts
+++ prj-directives-final/src/app/recipes/recipes.component.ts
@@ -12,9 +12,14 @@ import { RecipeService } from './recipe-list/recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor( private recipeService: RecipeService ) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );
   }
 
 }
