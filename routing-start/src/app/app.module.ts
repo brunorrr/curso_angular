ac70ae24cd48cf9ae9fdeab52311dda56ef122c5
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-03-10 23:03:31 -0300

    Implement First routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +12,20 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+
+/*
+  This is a constant array that contains all routes of our app,
+  It's a Routes type, inside it we have an object with two information about the route,
+  the path witch is the HTTP path that is showed in address bar,
+  the component witch is called when the path is set on URL
+
+  PS: this constant do not modify the app by itself, this needs to be used by NgModule decorator
+*/
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent }
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +40,12 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    /*
+      Importing RouterModule from Angular,
+      The module is get by the method forRoot inside RouterModule and we pass the appRoutes as an argument to this method
+    */
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
