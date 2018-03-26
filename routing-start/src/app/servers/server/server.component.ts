6bcfd781a7911e0ca00126ececa6970d250daac8
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-03-25 21:47:39 -0300

    Implement Server load by path param

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent implements OnInit, OnDestroy {
   server: {id: number, name: string, status: string};
+  routeObserver: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // Making sure that the ID is a number
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.routeObserver = this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(
+          +params['id']
+        );
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.routeObserver.unsubscribe();
   }
 
 }
