e59545bcac4b58d60be5efe98a723e969138f689
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-07-14 15:49:10 -0300

    prj-routing-final initial commit

---
+++ prj-routing-final/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,27 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+
+export class ShoppingListService {
+
+    ingredientChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ];
+
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+
+    addIngredient(newIngredient: Ingredient){
+        this.ingredients.push(newIngredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients( ingredients : Ingredient[]){
+        //Add all ingredients to ingredients list
+        this.ingredients.push(...ingredients);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
+}
