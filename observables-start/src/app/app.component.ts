3632f54edb20cef771b7fd0afdcab9f7e6ffed93
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-07-15 21:47:03 -0300

    Use subject as event emitter

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.userActivated.subscribe(
+      (id: number) => {
+        this.user1Activated = id === 1;
+        this.user2Activated = id === 2;
+      }
+    );
+  }
 }
