45353a9a5a9faddb68fdc655f86d86d9b96a890f
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-07-15 21:16:01 -0300

    Implement Observable.create

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 /*
   For Angular 5- use import { Observable } from 'rxjs/Observable';
 */
-import { interval } from 'rxjs';
+import { interval, Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,10 +14,37 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumbers = interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('First package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('Second package');
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('This does not work');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.error('This does not work');
+      }, 6000);
+    });
+    myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      // The complete method is always the last method called
+      () => {
+        console.log('completed');
       }
     );
   }
