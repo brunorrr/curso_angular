dda8b06e3ed3e9eef65c23defba7b721a164acb4
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-07-29 15:51:14 -0300

    Assignment done

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  defaultSubscription = 'advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
+
+  @ViewChild('f') signupForm: NgForm;
+
+  onSubmit() {
+    this.user.email = this.signupForm.value.email;
+    this.user.subscription = this.signupForm.value.subscription;
+    this.user.password = this.signupForm.value.password;
+
+    this.submitted = true;
+
+    // Reset everything in the form
+    this.signupForm.reset();
+  }
 
 }
