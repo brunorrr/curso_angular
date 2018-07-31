4b512be6a9b9ee1c907bb7ab0b230b604af71094
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-07-31 20:08:50 -0300

    Add Async validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
       'userData': new FormGroup({
         // You may wrap all attributes with single quote mark ' to help HTML code to identify this code
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
         'gender': new FormControl('male'),
         // FormArray allows you to create a list of form items
         'hobbies': new FormArray([])
@@ -42,6 +43,22 @@ export class AppComponent implements OnInit {
       return {'nameIsForbidden': true};
     }
     return null;
+  }
+
+  /**
+    Using a async method as custom validator
+    It returns a Promise to Angular with a check if email is equals to test@test.com
+  */
+  forbiddenEmails( control: FormControl ): Promise<any> | Observable<any> {
+    return new Promise<any>(( resolveMethod, reject) => {
+      setTimeout(() => {
+        if ( control.value === 'test@test.com') {
+          resolveMethod({'emailIsForbidden': true});
+        } else {
+          resolveMethod(null);
+        }
+      }, 1500);
+    });
   }
 
 }
