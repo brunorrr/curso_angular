d90046aea3325572afc202847cfa170ec2d49afc
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-07-31 19:26:51 -0300

    Add Form array

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +16,23 @@ export class AppComponent implements OnInit {
         // You may wrap all attributes with single quote mark ' to help HTML code to identify this code
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
-        'gender': new FormControl('male')
+        'gender': new FormControl('male'),
+        // FormArray allows you to create a list of form items
+        'hobbies': new FormArray([])
       })
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    // Adding a new form control to our form array
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('userData.hobbies')).push(
+      control
+    );
   }
 
 }
