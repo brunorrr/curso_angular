010a03a68aa01be0ee9ee2d37a8ec9dc81626fb6
Bruno Ricci <brunoriccidossantos@hotmail.com>
2018-08-05 18:46:23 -0300

    Implement Fetch data logic

--- prj-http-final/src/app/shared/data-storage.service.ts
+++ prj-http-final/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable()
@@ -9,5 +9,15 @@ export class DataStorageService {
   storeRecipes() {
     return this.http.put('https://ng-recipe-book-cc8a3.firebaseio.com/recipes.json',
       this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get('https://ng-recipe-book-cc8a3.firebaseio.com/recipes.json')
+      .subscribe(
+        (response: Response) => {
+          const recipes = response.json();
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
